24d5b26806200ee91184b428b90a7586ad69af63
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-14 22:35:03 +0200

    Isolate data access

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
-$db = new PDO ('mysql:host=localhost;dbname=blog;charset=utf8','root','');
-$req = $db->query('SELECT id, chapter_order, title, content, DATE_FORMAT(date_article, \'%d/%m/%Y à %Hh%imin\' ) AS date_fr FROM articles ORDER BY chapter_order DESC');
+include 'model.php';
+
+$req = getAllArticles();
 
 include 'indexView.php';
