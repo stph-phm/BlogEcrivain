235f291fbb54a6f8aa24cd9048442f805f41bf99
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-21 15:10:44 +0200

    Adding new functionality to the model: retrieving the last chapter, adding, updating and deleting an article

--- model/ArticleManager.php
+++ model/ArticleManager.php
@@ -36,4 +36,59 @@ class ArticleManager extends Manager
 
         return $article;
     }
+
+    /**
+     * Get last article in database articles 
+     * @return $article 
+     */
+    public function getLastArticle()
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, chapter_order, title, content, DATE_FORMAT(date_article, \'%d/%m/%Y à %Hh%imin\' ) AS date_fr FROM articles ORDER BY chapter_order DESC LIMIT 1');
+
+        $req->execute(array());
+        $article = $req->fetch();
+
+        return $article;
+    }
+
+    /**
+     * Create a new article 
+     * @param $title, $content
+     */
+    public function createArticle($title, $content)
+    {
+        $db = $this->dbConnect();
+        $reqArticle = $db->prepare('INSERT INTO articles(title,content, date_article) VALUES(:title, :content, NOW())');
+        $reqArticle->execute(array("title" => $title, "content" => $content));
+    }
+
+    /**
+     * Edit an article 
+     * @param $article_id (int), $title, $content
+     */
+    public function editArticle($article_id, $title, $content)
+    {
+        $db = $this->dbConnect();
+        $reqArticle = $db->prepare(
+            'UPDATE articles 
+            SET title= :title, content = :content, date_article = NOW() 
+            WHERE id = :id ');
+        $reqArticle->execute(array("id" => $article_id, "title" => $title, "content" => $content));
+    }
+
+    /**
+     * Delete an article 
+     * @param $article_id
+     */
+    public function deleteArticle($article_id)
+    {
+        $db = $this->dbConnect();
+
+        $reqArticle = $db->prepare('DELETE FROM articles WHERE id = ?'); 
+        $reqArticle->execute(array($article_id));
+
+        $reqArticle = $db->prepare('DELETE FROM comments WHERE id = ?'); 
+        $reqArticle->execute(array($article_id));
+    }
 }
