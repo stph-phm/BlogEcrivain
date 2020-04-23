d9e0efed3e916c9bf96f71ca61211e8cb2150347
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-23 19:26:57 +0200

    Update

--- index.php
+++ index.php
@@ -22,45 +22,17 @@ switch ($action) {
         $comment = new \OpenClassrooms\Blog\Controller\Comment;
         $comment->addComment($_GET['id'], $_POST['pseudo'], $_POST['comment']); // Changement de param $article_id, $pseudo, $comment => error ne reconnait pas les variables 
         break; 
-
+    case 'reportComment':
+        $comment = new \OpenClassrooms\Blog\Controller\Comment;
+        $comment->reportComment($_GET['id'], $_POST['report']);
+    case 'admin':
+        $dashboard = new  \OpenClassrooms\Blog\Controller\Article;        
+        $dashboard->managerReportComment();
+        break;
 
     default:
         $listArticle = new \OpenClassrooms\Blog\Controller\Article;
         $listArticle->allArticles();
         break;
 }
-
-// try {
-//     if (isset($_GET['action'])) {
-//         if ($_GET['action'] == 'allArticles') {
-//             allArticles(); //OK ! 
-//         } 
-//         elseif ($_GET['action'] == 'article') {
-//             if (isset($_GET['id']) && $_GET['id'] > 0 ) {
-//                 article();
-//             }
-//             else {
-//                 throw new Exception('Aucun identifiant de billet envoyé');
-//             }
-//         } // OK ! 
-        // elseif ($_GET['action'] == 'addComment') {
-        //     if (isset($_GET['id']) && $_GET['id'] > 0) {
-        //         if (!empty($_POST['pseudo']) && !empty($_POST['comment'])) {
-        //             addComment($_GET['id'], $_POST['pseudo'], $_POST['comment']);
-        //         }                 else {
-        //             throw new Exception('Tous les champs ne sont pas remplis !');
-        //         }             
-        //     }
-//             else {
-//                 throw new Exception('Aucun identifiant de billet envoyé');
-//             }
-//         }
-// }
-//     }
-//     else {
-//         allArticles();
-//     }   
-// }
-// catch (Exception $e) {
- //   echo 'Erreur :' .$e->getMessage();
 
