7b5f734e64b252441801880504ebe810ccea5489
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-23 19:26:25 +0200

    Login page creation

---
+++ view/frontend/connectView.php
@@ -0,0 +1,22 @@
+<?php $title = "Connect" ?>
+
+<?php ob_start(); ?>
+    <div class="global">
+        <h1>Se connecter </h1>
+
+        <form action="#" method="post">
+            <div>
+                <label for="username">Votre identifiant : </label>
+                <input type="text" name="username" id="username"> <br>
+                <label for="password">Votre mot de passe : </label>
+                <input type="password" name="password" id="password">
+            </div>
+                <input type="button" name="submit" value="Se connecter">
+        </form>
+    </div><!--/.global-->
+
+    <?php $content = ob_get_clean(); ?>
+    <?php include 'template.php'; ?>
+
+
+
