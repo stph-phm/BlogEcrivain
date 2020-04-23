9bfd88c018e390aeb73ee5e8f449acac28cd44e2
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-23 19:12:08 +0200

    Create a UserManager model class

---
+++ model/UserManager.php
@@ -0,0 +1,21 @@
+<?php
+namespace OpenClassrooms\Blog\Model;
+
+include_once 'model/Manager.php';
+
+class UserManager extends Manager 
+{
+    public $id_users;
+    public $username;
+    public $password_user;
+
+    public function username($username) {
+        $db =  $this->dbConnect();
+        $reqUser = $db->prepare('SELECT id, username, password_user,  DATE_FORMAT(date_user, \'%d/%m/%Y à %Hh%imin\' ) AS date_fr FROM  users');
+
+        $reqUser->execute(array($username));
+        $user = $reqUser->fetch();
+
+        return $user;
+    }
+}
