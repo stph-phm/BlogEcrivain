9f6242665eb0717573a4abe9b5a966b666fdf9df
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-27 20:38:04 +0200

    Change of method depending on the subject of the classes in Controller

--- controller/Comment.php
+++ controller/Comment.php
@@ -15,14 +15,16 @@ class Comment {
      * Check if the fiels are filled 
      * Call getAddComment method @param $_GET['id'], $_POST['pseudo'], $_POST['comment']
      */
-    public function addComment($article_id, $pseudo, $comment)
+    public function addComment()
     {
         $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+        $article_id = trim($_GET['id']);
+        $pseudo     =   trim(htmlspecialchars($_POST['pseudo']));
+        $comment    = trim(htmlspecialchars($_POST['comment']));
 
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             if (!empty($_POST['pseudo']) && !empty($_POST['comment'])) {
-                
-                $addLinesComment = $commentManager->getAddComments($_GET['id'], $_POST['pseudo'], $_POST['comment']);
+                $addLinesComment = $commentManager->getAddComments($article_id, $pseudo, $comment);
                 
                 header('Location: index.php?action=article&id=' .$article_id);
             }
@@ -43,6 +45,7 @@ class Comment {
 
         if (isset($_GET['id']) && $_GET['id'] > 0 ) {
             if (isset($_POST['report'])) {
+                $article_id = 
                 $reportComment = $commentManager->reportComments($_GET['id'], $_POST['report']);
 
             header('Location: index.php?action=article&id=' .$article_id);
@@ -51,5 +54,31 @@ class Comment {
 
         }
     }
+
+    // Gestion des commentaires signalée
+    // ignorer ou supprimer les commentaires signalés
+    public function managerReportComment()
+    {
+        // verifie si le button ignore existe 
+        // lorsqu'on click sur le button ignore => le commentaire ignorer ne sera plus signalé (il s'affiche plus dans le tableau)
+        // redirige le lien tableau de bord ? 
+        $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+        if (isset($_POST['ignore'])) {
+            $ignored = $commentManager->ignoreComments($comment_id);
+
+            header('Location: index.php?action=admin');
+        }
+
+        // Vérifie si le button delete comment existe
+        // lorsqu'on click sur le button delete => supprime le commentaire 
+        // redirige le lien tableau de bord ? 
+        if (isset($_POST['deleteComment'])) {
+            $deleteComment = $commentManager->deleteComments($comment_id);
+            
+            header('Location: index.php?action=admin');
+        }
+        include 'view/frontend/adminView.php';
+    }
+
 
 }
