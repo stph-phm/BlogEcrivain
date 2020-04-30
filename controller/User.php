aea15fd5493f6990b0ca3271ddb67031f1d60743
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-30 02:32:38 +0200

    Edit name file

--- controller/User.php
+++ controller/User.php
@@ -1,15 +1,23 @@
 <?php
 namespace App\Blog\Controller;
 
+use App\Blog\Model\UserManager;
+use App\Blog\Model\ArticleManager;
+use App\Blog\Model\CommentManager;
+
 include_once 'model/UserManager.php';
 
 class User {
      // Connecter 
     public function login() 
     {
-        $userManager = new \OpenClassrooms\Blog\Model\UserManager();
-        $userManager->
-        header('Location: index.php?action=admin');
+        $userManager = new UserManager();
+        $userManager->username($username);
+
+     
+            header('Location: index.php?action=admin');
+        
+       
         // vérifie si le button login existe 
         // Test si les champs sont remplis
         // verifie si le $_POST['username'] == username de la BDD  && $_POST['password'] == password_user de la BDD 
@@ -23,9 +31,13 @@ class User {
         //Affiche un tableau avec tous les chapitres et les actions (voir, modifier et supprimer)
         // Un autre tableau avec tous les commentaires signalés avec les actions (ignorer et supprimer)
         // un bouton d'ajouter un nouveau article => envoie une nouvelle page (formulaire sous forme TinyMCE)
-        $articleManager = new \OpenClassrooms\Blog\Model\ArticleManager();
+        $i = 1;
+        $articleManager = new ArticleManager();
+        $commentManager = new CommentManager();
         $dashboard = $articleManager->getAllArticles();
+        $dashboard = $commentManager->getAllReported();
         include 'view/adminView.php';
+
     }
 
 
