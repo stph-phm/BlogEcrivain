1d07e262511be6cbd16faab7b3a9ad1f1d344152
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-06 19:14:27 +0200

    Change name Bdd

--- model/Manager.php
+++ model/Manager.php
@@ -9,7 +9,7 @@ class Manager
      */
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname=blog1;charset=utf8', 'root', '');
+        $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
         return $db;
     }
 }
