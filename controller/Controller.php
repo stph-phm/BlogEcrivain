1f414fda89f57fd7ace9f4646f1764b91b1a3266
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-15 01:30:23 +0200

    Added new empty function in the Controller class

--- controller/Controller.php
+++ controller/Controller.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller; 
 
+use App\Model\UserManager;
+
 class Controller {
     // methodes pour savoir si le membre est connecter 
     public function is_connected()
     {
-        
+        $userManager = new UserManager();
+
     }
 
     // méthode pour savoir si il est admin 
     public function is_admin()
     {
+        $userManager = new UserManager();
+        $listUserAdmin = $userManager->getAllUserAdmin();
 
     }
 }
