9c335287f3a84ebae60d89cb436c1ac4f54b6610
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-20 18:36:57 +0200

    Add other methods to secure the variables for the controller

--- controller/Controller.php
+++ controller/Controller.php
@@ -5,18 +5,26 @@ namespace App\Controller;
 use App\Model\UserManager;
 
 class Controller {
+    public $tring;
     // methodes pour savoir si le membre est connecter 
     public function is_connected()
     {
         $userManager = new UserManager();
-
     }
 
     // méthode pour savoir si il est admin 
     public function is_admin()
     {
         $userManager = new UserManager();
-        $listUserAdmin = $userManager->getAllUserAdmin();
+    }
 
+    public function str_secur($string)
+    {
+        return trim(htmlspecialchars($string));
+    }
+
+    public function trim_secur($string)
+    {
+        return trim($string);
     }
 }
