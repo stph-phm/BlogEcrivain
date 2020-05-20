27a7a66f282c85a27a64ebd7c4fb9a962d09121f
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-20 18:36:04 +0200

    Modification of the Usermanager class to own

--- model/UserManager.php
+++ model/UserManager.php
@@ -9,114 +9,72 @@ class UserManager extends Manager
 {
     public $user_id;
     public $username;
-    public $status_user;
+    public $is_admin; 
     public $email_user;
     public $password_user;
-    
 
-
-    /**
-     * Get all the users of the database to connect 
-     * @param $email_user, $password_user
-     * @return $userExist
-     */
-    public function loginUser($email_user, $password_user) {
-        $db =  $this->dbConnect();
-        $reqUser = $db->prepare('SELECT * FROM  users WHERE email_user = :email_user AND password_user = :password_user');
-
-        $reqUser->execute(array(":email_user" => $email_user, ":password_user" => $password_user));
-
-        $userExist = $reqUser->rowCount();
+    public function getUsernameExist($username)
+    {
+        $db = $this->dbConnect();
+        $reqUsername = $db->prepare('SELECT * FROM users WHERE username = :username');
+        $reqUsername->execute([
+            'username' => $username
+            ]);
+        $userExist = $reqUsername->rowCount();
 
         return $userExist;
     }
 
-        public function getProfilUser()
+    public function getMailExist($email_user)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT * FROM  users WHERE id = ?');
-
-        $reqUser->execute(array());
-
-        $userInfo = $reqUser->fetch();
-
-        return $userInfo;
+        $reqMail = $db->prepare('SELECT * FROM users WHERE email_user = :email_user');
+        $reqMail->execute([
+            'email_user' => $email_user
+        ]);
+        $mailExist = $reqMail->rowCount();
+        
+        return $mailExist;
     }
 
-    /**
-     * Add users to the database users 
-     * @param $username, $email_user, $password_user
-     * @return $insertUser
-     */
-    public function addUser($username,$email_user, $password_user) 
+    public function insertNewUser($username,$email_user, $password_user)
     {
         $db = $this->dbConnect();
-        $reqUser = $db->prepare('INSERT INTO users(username,email_user, password_user, date_user) VALUES (:username,:email_user, :password_user, NOW())');
-        
-        $insertUser = $reqUser->execute(array(":username"  => $username,":email_user" => $email_user, ":password_user" => $password_user));
+        $reqUSer = $db->prepare('INSERT INTO users(username, email_user, password_user, date_user) VALUES (:username, :email_user, :password_user,NOW())');
+        $insertUser = $reqUSer->execute([
+            'username'          => $username,
+            'email_user'        => $email_user, 
+            'password_user'     => $password_user 
+        ]);
 
         return $insertUser;
     }
 
-    /**
-     * Retrieves all the emails already existing in the database users 
-     * @param $email_user
-     * @return $mailExist
-     */
-    public function checkMail($email_user)
+    public function getUserbyMail($email_user)
     {
         $db = $this->dbConnect();
-        $reqMail = $db->prepare('SELECT * FROM users where  email_user = ?');
+        $reqMail = $db->prepare('SELECT *  FROM users WHERE email_user = :email_user');
+        $reqMail->execute([
+            'email_user' => $email_user
+        ]);
 
-        $reqMail->execute(array($email_user));
+        $user = $reqMail->fetch();
 
-        $mailExist = $reqMail->rowCount();
-
-        return $mailExist;
+        return $user;
     }
 
-
-
-    /**
-     * Changement de status de visiteur à admin
-     */
-    public function statusChangeInAdmin($status_user) 
+    public function getUser($user_id)
     {
         $db = $this->dbConnect();
-        $reqUser = $db->prepare('UPDATE users SET status_user = 1');
-        $reqUser->execute(array("status_user" => $status_user));
-    } 
+        $reqUSer = $db->prepare('SELECT * FROM user WHERE id = :id');
+        $reqUSer->execute([
+            'id' => $user_id
+        ]);
 
-    /**
-     * Changement de status Admin a visiteur
-     */
-    public function statusChangeAdminToVisitor($status_user)
-    {
-        $db = $this->dbConnect();
-        $reqUser = $db->prepare('UPDATE users SET status_user = 0');
-        $reqUser->execute(array("status_user" => $status_user));
-    } 
+        $userInfo = $reqUSer->fetch();
 
-    public function getAllUserAdmin()
-    {
-        $db = $this->dbConnect();
-        $reqUser = $db->query('SELECT * FROM users WHERE status_user = 1');
-
-        $listUserAdmin = $reqUser->fetchAll();
-        return $listUserAdmin;
+        return $userInfo;
     }
 
-
-    /**
-     * Récupère tous les user non admin 
-     */
-    public function getAllVisitor()
-    {
-        $db = $this->dbConnect();
-        $reqUser = $db->query('SELECT * FROM users WHERE status_user = 0');
-
-        $listVisitor = $reqUser->fetchAll();
-        return $listVisitor;
-    }
 
 }
