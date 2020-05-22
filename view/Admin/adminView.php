93fcf28271e2bc980fe2fe468dd02734d4fd12af
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-22 02:20:39 +0200

    edit Page admin

--- view/Admin/adminView.php
+++ view/Admin/adminView.php
@@ -1,35 +1,38 @@
 <?php $title = "Tableau de bord ";  ?>
 <?php ob_start(); ?>
-<div class="global dashboard ">
-    
-    <h1>Bonjour</h1>
+<div class="Ajouter un nouvel article ">
+    <div class="global">
+        <h1>Bonjour </h1>
     <p><a href="index.php">Retour index</a></p>
     <div class="container-dashboard">
+        <div class="title_admin">
+            <h2 class="text-center mb-2">Les commentaires signalée </h2>
+            <a href="index.php?action=createArticle" class="btn btn-success mb-4"> Ajouter un article <span><i class="fas fa-plus"></i> </span></a>
+        </div>
 
-        <table class="table ">
-            <thead>
+        <table class="table">
+            <thead class="table">
                 <tr>
                     <th>#</th>
-                    <th>Appartien à quel titre de chapitre : </th>
-                    <th> Contenue du commentaire : </th>
-                    <th>Action </th>
+                    <th>Pseudo </th>
+                    <th wi> Contenue du commentaire </th>
+                    <th width=300>Action </th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($dashboard as $commentReported) { ?>
+                <?php foreach ($listCommentsReport as $listReported) { ?>
                 <tr>
                     <td> <?= $i++ ?></td>
-                    <td> <?= htmlspecialchars($allArticles['title']) ?> </td>
-                    <td> <?= nl2br(substr($allArticles['content'],0,150)) ?></td>
-                    <td> <?= $allArticles['date_fr'] ?> </td>
-                    <td width=500>
-                        <form action="" method="post">
-                            <button type="submit">Valider</button>
-                            <button type="submit">Supprimer</button>
-                        </form>
+                    <td> <?= htmlspecialchars($listReported['pseudo']) ?> </td>
+                    <td> <?= nl2br(substr($listReported['comment'],0,150)) ?></td>
+                    <td class="table-action">
+                        <a href="index.php?action=validateReported&amp;id=<?= $listReported['id'] ?>"
+                            class="btn btn-primary"> Valider <i class="fas fa-check"> </i></a>
+
+                        <a href="index.php?action=deleteCommentReport&amp;id= <?= $listReported['id'] ?>"
+                            class="btn btn-danger">Supprimer <i class="fas fa-times"></i></a>
                     </td>
                 </tr>
-
                 <?php 
                 }   
                 ?>
@@ -38,6 +41,8 @@
     </div>
 </div>
 
+    </div>
+    
 
 
 <?php $content = ob_get_clean(); ?>
