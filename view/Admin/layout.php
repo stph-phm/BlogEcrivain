cc851b6df0a3484f886b4556eeb68acc81decebe
Stph phm <stephanie.pham.lnd@gmail.com>
2020-05-22 02:22:11 +0200

    edit profil page

--- view/Admin/layout.php
+++ view/Admin/layout.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- FONTAWESOME -->
+        <script src="https://kit.fontawesome.com/effe484f35.js" crossorigin="anonymous"></script>
     <title> <?= $title; ?> </title>
 </head>
 
