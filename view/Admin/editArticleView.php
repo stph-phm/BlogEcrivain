6b7ffd06bd6cb2bf01bde9b9fd96827b8161f7ae
Stph phm <stephanie.pham.lnd@gmail.com>
2020-08-12 22:25:18 +0200

    improvement of the Controller class, layout of Media Queries, correction of deletion of comments by the admin on the frontend

---
+++ view/Admin/editArticleView.php
@@ -0,0 +1,20 @@
+<?php $title = "Modifier le chapitre :" ;  ?>
+<?php ob_start(); ?>
+
+<div class="global dashboard ">
+<a href="index.php?action=manageArticle"> Retour à la gestion des articles</a>
+    <h1 class="text-center">Modifier un article </h1>
+    <form action="index.php?action=edit&amp;id= <?= $article['id'] ?>" method="post">
+        <div class="form-group">
+            <label for="title">Titre du chapitre</label>
+            <input type="text" class="form-control" id="title" name="title" value="<?= $article['title'] ?>">
+        </div>
+        <div class="form-group">
+            <label for="content">Contenue du chapitre </label>
+            <textarea name="content" id="default" class="form-control" rows="20"> <?= $article['content'] ?>  </textarea>
+        </div>
+        <button class="btn btn-primary" type="submit" name="submit">Modifier</button>
+    </form>
+</div>
+<?php $content = ob_get_clean(); ?>
+<?php include 'view/template.php'; ?>
