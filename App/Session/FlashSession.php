eb8a245f546ce0c1528031ffe166f2340d4aaf37
Stph phm <stephanie.pham.lnd@gmail.com>
2020-08-14 13:03:48 +0200

    Correction Message Flash

--- App/Session/FlashSession.php
+++ App/Session/FlashSession.php
@@ -5,10 +5,11 @@ namespace App\Session;
 class FlashSession {
 
     
-
+    //permet de récupérer les messages du tableau $_Session
      public function get() {
 
-           $flash = $_SESSION['flash'] = array();
+         //récupère la valeure de $_Session['flash'] et renvoie un tableau vie s'il n'existe pas
+         $flash = $_SESSION['flash'] ?? [];
 
           if ($_SESSION['flash'] = false) {
               unset($_SESSION['flash']);
@@ -18,12 +19,16 @@ class FlashSession {
 
      // Permet d'ajouter un message dans le tableau Flash de la Session
      public function set($type , $message) {
-          $_SESSION['flash']= [
-              'type'        => $type,
-              'message'     => $message
-          ];
-     }
+         //récupère la valeure de $_Session['flash'] et renvoie un tableau vie s'il n'existe pas
+         $flash = $_SESSION['flash'] ?? [];
 
+         $flash[] = [
+             'type' => $type,
+             'message' => $message
+         ];
+
+         $_SESSION['flash'] = $flash;
+     }
 
 }
 
